214936a923847077bf7db17068ed2b8e18877f18
Nick Brown <nwbrown@gmail.com>
2019-12-15 19:17:43 -0500

    some cleanup

--- config/dev.py
+++ config/dev.py
@@ -4,5 +4,5 @@ QUEUE = {"TYPE": env.QUEUE_TYPE_MEMORY}
 PARSER = {"TYPE": env.PARSER_TYPE_BAG_OF_WORDS}
 DB = {"CONNECTION_STRING": "sqlite:///db.sql"}
 DB = {"CONNECTION_STRING": "sqlite:///{file}", "CONN_PARAMS": {"file": "dev.sql"}}
-MODEL = {"SAVE_LOC": "./models", "MAX_DOCS": 100}
+MODEL = {"SAVE_LOC": "./", "MAX_DOCS": 100}
 SCORER = {"EMBEDDED": True, "SLEEP_TIME": 60}
